2293396711d775458bea711de1510563f28d80dc
sreenidhipyla286-ctrl <sreenidhipyla286@gmail.com>
2026-03-03 08:39:27 +0530

    THIS IS MY FIRST COMMIT

---
+++ OOPSBannerApp.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp
+ * 
+ * UC1: Print the literal text "OOPS" to the console.
+ * 
+ * @author Nidhi
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+		System.out.println("This is Main Branch");
+    }
+}
